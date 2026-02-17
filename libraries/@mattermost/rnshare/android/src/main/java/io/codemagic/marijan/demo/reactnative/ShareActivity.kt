674b0e803b1a1173902bcaa7f7c49db3d3323a52
Marijan Kozic <marijan@todah.hr>
2026-02-17 12:57:09 +0100

    delete old files

--- libraries/@mattermost/rnshare/android/src/main/java/io/codemagic/marijan/demo/reactnative/ShareActivity.kt
+++
@@ -1,9 +0,0 @@
-package io.codemagic.marijan.demo.reactnative.rnshare
-
-import com.facebook.react.ReactActivity
-
-class ShareActivity : ReactActivity() {
-    override fun getMainComponentName(): String {
-        return "MattermostShare"
-    }
-}
--- libraries/@mattermost/secure-pdf-viewer/android/src/main/java/io/codemagic/marijan/demo/reactnative/securepdfviewer/pdfium/shared/PdfViewInterface.kt
+++
@@ -1,7 +0,0 @@
-package io.codemagic.marijan.demo.reactnative.securepdfviewer.pdfium.shared
-
-interface PdfViewInterface {
-    val viewWidth: Int
-    val viewHeight: Int
-    fun invalidate()
-}
